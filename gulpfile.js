c23b600f7a26c59376bfbe75b6e9e43b35664771
Jonathan Newman <jonathan@overfoc.us>
2016-12-19 05:09:07 -0600

    moved public assets into public/ folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ var autoprefixer = require('gulp-autoprefixer');
 gulp.task('default', ['build-styles']);
 
 gulp.task('watch', function() {
-  gulp.watch(['styles/**/*.*'], ['build-styles']);
+  gulp.watch(['public/styles/**/*.*'], ['build-styles']);
 });
 
 gulp.task('build-styles', function() {
-  return gulp.src(['./styles/style.scss'])
+  return gulp.src(['./public/styles/style.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 3 versions', '> 5%', 'Firefox > 3', 'ie >= 8']
     }))
-    .pipe(gulp.dest('./styles'));
+    .pipe(gulp.dest('./public/styles'));
 });
