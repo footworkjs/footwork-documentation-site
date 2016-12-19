018b61ace2fe1e56b6de5ce5bdb6f1afee15b9f2
Jonathan Newman <jonathan@overfoc.us>
2016-12-19 14:25:57 -0600

    cleaner layout based on bootstrap 4 example, added logo

--- public/index.php
+++ public/index.php
@@ -11,22 +11,27 @@ function scanForReleases($releaseFolder) {
   </head>
 
   <body>
-    <nav class="navbar navbar-light bg-faded">
-      <ul class="nav navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="http://footworkjs.com">FootworkJS Home Page</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Latest Docs</a>
-        </li>
-      </ul>
+    <nav class="navbar navbar-dark bg-inverse fg-white">
+      <div class="container">
+        <a class="navbar-brand" href="http://footworkjs.com"><img src="/images/footwork-logo.png" /></a>
+        <span class="navbar-text saying">A solid footing for web applications.</span>
+      </div>
     </nav>
 
-    <div class="releases">
-      <?php foreach (scanForReleases('./release') as $folder): ?>
-        <a class="release" href="release/<?=$folder?>/"><?=$folder?></a>
-      <?php endforeach; ?>
-    </div>
+    <main class="container">
+      <p>Below is a listing of the available documentation releases:</p>
+
+      <div class="releases">
+        <?php foreach (scanForReleases('./release') as $folder): ?>
+          <a class="release" href="release/<?=$folder?>/"><?=$folder?></a>
+        <?php endforeach; ?>
+      </div>
+    </main>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
   </body>
 </html>
 
